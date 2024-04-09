e1dcc5d2cd5b4b6804c647583ee6ce7e2ee8197c
Rostyslav Krechun <rkrechun@c2s17d102.42wolfsburg.de>
2024-04-09 18:38:00 +0200

    exepermet with mlx and trying understanding

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: rkrechun <rkrechun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:00:56 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/04/04 17:05:07 by rkrechun         ###   ########.fr       */
+/*   Updated: 2024/04/09 16:18:32 by rkrechun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define SO_LONG_H
 
 #include "mlx/mlx.h"
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #endif
--- testhooksdestoy.c
+++ testhooksdestoy.c
@@ -1,21 +1,18 @@
-// /* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   testhooksdestoy.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkrechun <rkrechun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 15:58:13 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/04/09 17:03:02 by rkrechun         ###   ########.fr       */
+/*   Created: 2024/04/09 17:27:49 by rkrechun          #+#    #+#             */
+/*   Updated: 2024/04/09 17:27:51 by rkrechun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "so_long.h"
 
 typedef struct s_data{
 	void	*mlx;
 	void	*mlx_win;
-	void	*img;
 	
 }			t_data;
 
@@ -27,16 +24,39 @@ int key_cross(t_data *data)// close windows when i press red cross
 
 int	key_hook(int keycode, t_data *data)
 {
-	if (keycode == 53)// exit when press ESC
-	{
+	int		x = 10;
+	int		y = 10;
+	if (keycode == 53){// exit when press ESC
 		//printf("Hi , the key code of this bitton is : %d\n", keycode);
 		mlx_destroy_window(data->mlx, data->mlx_win);
 		exit(0);
 	}
-	if (keycode == 2)
+	if (keycode == 13)// print top line on square
 	{
-		mlx_put_image_to_window(data->mlx, data->mlx_win, &data->img, 64, 64);
-
+		while (x <= 110)
+		{	mlx_pixel_put(data->mlx, data->mlx_win, x, y, 0x00FF0000);
+			x++;}
+	}
+	if (keycode == 0) // print left line on  square
+	{	while (y <= 110)
+		{	mlx_pixel_put(data->mlx, data->mlx_win, x, y, 0x00FF0000);
+			y++;}
+	}
+	if (keycode == 1) // print bootom line on square
+	{
+		x = 110;
+		y = 110;
+		while(x >= 11)
+		{mlx_pixel_put(data->mlx, data->mlx_win, x, y, 0x00FF0000);
+		x--;}
+	}
+	if (keycode == 2) // print right line on square
+	{
+		y = 110;
+		x = 110;
+		while(y >= 11)
+		{	mlx_pixel_put(data->mlx, data->mlx_win, x, y, 0x00FF0000);
+		y--;}
 	}
 	return (0);
 }
@@ -56,21 +76,11 @@ int	key_hook(int keycode, t_data *data)
 int main(void)
 {
 	t_data	data;
-	data.img = calloc();
-
 	
 	data.mlx = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx, 1280, 960, "Morfik");
-
-	int w = 64;
-	int h = 64;
-	char	*relative_path = "../textures/pixil-frame-0.xpm";
-
-	data.img = mlx_xpm_file_to_image(data.mlx, relative_path, &w, &h);
-
-	mlx_xpm_file_to_image(data.mlx, relative_path, &w, &h);
-
+	
 	mlx_key_hook(data.mlx_win, key_hook, &data);// reading keys
-	mlx_hook(data.mlx_win, 17, 1L<<0, key_cross, &data);// waiting when someone press 
+	mlx_hook(data.mlx_win, 17, 1L<<0, key_cross, &data);// waiting when someone press red cross
 	mlx_loop(data.mlx);
 }
